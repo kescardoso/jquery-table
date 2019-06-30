79cc79203903ed71e14c29e5c38cf93871ad5610
Kes Cardoso <kescardoso@gmail.com>
2019-06-30 21:52:55 +0000

    Challenge completed

--- js/my-script.js
+++ js/my-script.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
+    
     $("tr:odd").addClass("odd");
     $("tr:even").addClass("even");
-})
+    
+    /*when table header is clicked ,
+    removes selection class from all other table rows and
+    adds the selection class to this table headers row only*/
+    $("th").click(function(){
+     	$("tr").children().removeClass("select");
+     	$(this).siblings().addClass("select");
+    });
+
+     	
+    
+        
+    // Challenge C - Chaneg Reset
+    // will display rgb value of selected panel in the reset panel
+    $(".theButton").click(function(){
+        var col = $(this).css("background-color");
+        console.log(col);
+        $(".superButton").text(col);
+    });
+        
+});
